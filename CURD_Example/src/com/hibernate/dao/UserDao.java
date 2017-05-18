83eae802d9cb8ba647e2dfb252c3a6fb21bf2245
sunil965clear <sunilkumar192114@gmail.com>
2017-05-18 16:29:50 +0530

    CURD operation with HQL

---
+++ CURD_Example/src/com/hibernate/dao/UserDao.java
@@ -0,0 +1,143 @@
+package com.hibernate.dao;
+
+import java.util.*;
+
+import org.hibernate.*;
+
+import com.hibernate.model.User;
+import com.hibernate.util.HibernateUtil;
+
+public class UserDao {
+
+	public void addUser(User user) {
+		Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			session.save(user);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			if (trns != null) {
+				trns.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+	}
+
+	public void deleteUser(int userid) {
+		Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			/*
+			 * User user = (User) session.load(User.class, new Integer(userid));
+			 * session.delete(user);
+			 */
+
+			Query query = session.createQuery("delete from User where id="+userid);
+			query.executeUpdate();
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			if (trns != null) {
+				trns.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+	}
+
+	public void updateUser(String email, int id) {
+		System.out.println("Update() is called");
+		Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			Query q=session.createQuery("update User set email=:n where id=:i");  
+			q.setParameter("n",email);  
+			q.setParameter("i",id);  
+			int status=q.executeUpdate();  
+			System.out.println(status); 
+			/*session.update(user);*/
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			if (trns != null) {
+				trns.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+			System.out.println("Update() is completed");
+
+		}
+	}
+
+	public List<User> getAllUsers() {
+		System.out.println("AllUsers() is called");
+		List<User> users = new ArrayList<User>();
+		Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			users = session.createQuery("from User").list();
+			for(User u:users){
+				System.out.println(u);
+			}
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+			System.out.println("AllUsers() is completed");
+
+		}
+		return users;
+	}
+
+	public User getUserById(int userid) {
+		System.out.println("Single user method is called");
+		User user = null;
+		Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			String queryString = "from User where id = :id";
+			Query query = session.createQuery(queryString);
+			query.setInteger("id", userid);
+			user = (User) query.uniqueResult();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+			System.out.println("Single user method is completed");
+
+		}
+		return user;
+	}
+	
+	public void selectWhere(int id) {
+		System.out.println("SELECT_WHERE STARTS");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trns = null;
+		try{
+			session.beginTransaction();
+			Query query = session.createQuery("select u.firstName, u.lastName, u.email from User u where u.id =:id");
+			query.setParameter("id", id);
+			Object[] abc = (Object[]) query.uniqueResult();
+			System.out.println(abc[0]+"   "+abc[1]+"   "+abc[2]);
+		}catch (RuntimeException e) {
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+
+		System.out.println("SELECT_WHERE STOPED");
+
+	}
+}
---
+++ CURD_Example/src/com/hibernate/main/App.java
@@ -0,0 +1,47 @@
+package com.hibernate.main;
+
+import com.hibernate.dao.UserDao;
+import com.hibernate.model.User;
+
+public class App {
+
+	public static void main(String[] args) {
+		UserDao dao = new UserDao();
+
+		// Add new user
+		User user = new User();
+		user.setFirstName("Manoj");
+		user.setLastName("Pawne");
+		user.setEmail("amit@gmail.com");
+		// dao.addUser(user);
+
+		// Update user with HQL Query
+//		dao.updateUser("manoj@yahoo.com", 1);
+
+		// Update user with Without HQL Query
+		/*
+		 * user.setEmail("sunil@updatedJdbc.com"); user.setUserid(1);
+		 * dao.updateUser(user);
+		 */
+
+		// Delete user
+		// dao.deleteUser(4);
+
+		// Get all users with HQL Query
+		dao.getAllUsers();
+
+		// Get all users without HQL Query
+		/*
+		 * for (User iter : dao.getAllUsers()) { System.out.println(iter); }
+		 */
+
+		// Get user by id
+		/*
+		 * System.out.println(dao.getUserById(8));
+		 */
+		
+		dao.selectWhere(1);
+
+	}
+
+}
